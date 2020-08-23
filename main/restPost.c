bee46f2aacc0a7115ccf5fcf4e17d91956cdf347
LeHuman <47549331+LeHuman@users.noreply.github.com>
2020-08-22 19:47:24 -0500

    clean

--- main/restPost.c
+++ main/restPost.c
@@ -3,12 +3,13 @@
 #include "esp_netif.h"
 #include "esp_system.h"
 #include "esp_tls.h"
-#include "freertos/FreeRTOS.h"
-#include "freertos/queue.h"
-#include "freertos/task.h"
 #include "nvs_flash.h"
 #include <stdlib.h>
 #include <string.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include "freertos/task.h"
 
 #include "esp_http_client.h"
 
